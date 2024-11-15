685eb4d0f4007d428cf8650cc9df5ee80d3d1108
jenia petrusenko <medvijenia@icloud.com>
2024-11-15 14:43:03 +0200

    restructure

--- src/infra/contants.py
+++ src/infra/contants.py
@@ -3,4 +3,4 @@ import os
 
 GLOBAL_PATH = os.path.abspath(__file__)
 PROJECT_PATH = GLOBAL_PATH.split('src')[0][:-1]
-JSON_DATA = fr'{PROJECT_PATH}\src\files\person_data.json'
+JSON_DATA = fr'{PROJECT_PATH}\src\infra\files\person_data.json'
