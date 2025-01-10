6f437f5a65681903c4a0e7194f04975f1edb668e
jenia petrusenko <medvijenia@icloud.com>
2025-01-10 14:16:40 +0200

    ingress

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+minikube start
+minikube enable addons ingress
+
+MINIKUBE_IP=$(minikube ip)
+ENTRY="$MINIKUBE_IP app.local.com"
+
+if ! grep -qxF "$ENTRY" /etc/hosts; then
+  echo "$ENTRY" | sudo tee -a /etc/hosts
+  echo "Entry added: $ENTRY"
+else
+  echo "Entry already exists: $ENTRY"
+fi
+grep -qxF "$(minikube ip) ai-bot.local.com" /etc/hosts || echo "$(minikube ip) ai-bot.local.com" | sudo tee -a /etc/hosts
+kubectl apply -f .
+curl http://app.local.com/health
